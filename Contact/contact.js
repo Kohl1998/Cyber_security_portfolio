fea140849e08b129fa91f6a5f94b466e6e044263
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2024-01-09 15:36:20 +0000

    updated script

--- Contact/contact.js
+++ Contact/contact.js
@@ -3,20 +3,19 @@ const form = document.querySelector('.contact-form')
 
 
 function sendMSG(e) {
-    
-    const email = document.querySelector('.email')
-    const message = document.querySelector('.message')
-    
     e.preventDefault()
+    email = document.querySelector('.email')
+    message = document.querySelector('.message')
+
     Email.send({
-        SecureToken : "658c7ef7-c558-42b3-8808-8a76e5109423",
-        To : 'kohlwebdev@gmail.com',
-        From : email.value,
-        Subject : "Contact Form",
-        Body : message.value
+        SecureToken: "658c7ef7-c558-42b3-8808-8a76e5109423",
+        To: 'kohlwebdev@gmail.com',
+        From: email.value,
+        Subject: "Contact Form",
+        Body: message.value
     }).then(
         message => alert(message)
-        ); 
-    }
-    
+    );
+}
+
 form.addEventListener('submit', sendMSG)
