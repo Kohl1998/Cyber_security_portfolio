2af193371e0ef073be348df06186dc0e08235fe1
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2024-01-09 16:08:50 +0000

    added host to object

--- Contact/contact.js
+++ Contact/contact.js
@@ -7,10 +7,11 @@ function sendMSG(e) {
     message = document.querySelector('.message')
 
     Email.send({
+        Host: "smtp.gmail.com",
         SecureToken: "658c7ef7-c558-42b3-8808-8a76e5109423",
-        To: 'kohlwebdev@gmail.com',
         username: "kohlwebdev@gmail.com",
         password: "50FA902495944ABD5D7ED0FA81289496F4C2",
+        To: 'kohlwebdev@gmail.com',
         From: email.value,
         Subject: "Contact Form",
         Body: message.value
