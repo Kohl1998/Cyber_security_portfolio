5b3d7a77f66df932d0271393fd9e52bd976304aa
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2024-01-09 15:09:47 +0000

    JSON file

---
+++ Projects/projects.js
@@ -0,0 +1,29 @@
+var Data = [{
+    "id": 1,
+    "title": "Updating Linux permissions",
+    "summary": "In this project as a security Analyst I used the CIA model by updating permissions for files/directories, ensuring owners, groups, and others had the tools needed to carry out their work."
+}, {
+    "id": 2,
+    "title": "Querying SQL",
+    "summary": "For this project I had to perform a SQL query to get data on operating systems that need updating for security purposes."
+},
+{
+    "id": 3,
+    "title": "Running NMAP scan",
+    "summary": "For this project I installed NMAP through Linux and ran a scan on my IP address to see all devices on the network",
+},
+{
+    "id": 4,
+    "title": "Analysing network traffic with Snort",
+    "summary": "For this I utilized Snort to check for any intrusions on the network"
+}
+]
+
+
+
+
+
+
+
+
+
